855e083dfefbd8cf957154379cd1dc97e46f450c
chenq <chenq@ktc.cn>
2020-06-06 17:23:09 +0800

    eureka和zuul

---
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenqiang
+ * @create 2020-06-06 14:22
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ ad-gateway/src/main/java/com/imooc/ad/ZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author chenqiang
+ * @create 2020-06-06 17:20
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
