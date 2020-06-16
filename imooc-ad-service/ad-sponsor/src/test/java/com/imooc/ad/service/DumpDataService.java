7e9a553c42413c1b7dbe474416ff4f310da85d98
chenq <chenq@ktc.cn>
2020-06-16 15:35:17 +0800

    实现导出数据库数据到文件

--- imooc-ad-service/ad-sponsor/src/test/java/com/imooc/ad/service/DumpDataService.java
+++ imooc-ad-service/ad-sponsor/src/test/java/com/imooc/ad/service/DumpDataService.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
