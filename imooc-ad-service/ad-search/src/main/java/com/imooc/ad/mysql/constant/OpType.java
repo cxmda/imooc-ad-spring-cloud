3c794d37dc6a92b5b6618130500b6e4fe7d2d07e
chenq <chenq@ktc.cn>
2020-06-16 19:28:41 +0800

    ad-search之索引相关

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/OpType.java
@@ -0,0 +1,12 @@
+package com.imooc.ad.mysql.constant;
+
+/**
+ * @author chenqiang
+ * @create 2020-06-16 15:47
+ */
+public enum  OpType {
+    ADD,
+    UPDATE,
+    DELETE,
+    OTHER;
+}
--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/utils/CommonUtils.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/utils/CommonUtils.java
@@ -15,4 +15,14 @@ public class CommonUtils {
     public static <K,V> V getorCreate(K key, Map<K,V> map, Supplier<V> factory){
         return map.computeIfAbsent(key,k -> factory.get());
     }
+
+    public static String stringConcat(String... args){
+        StringBuilder builder = new StringBuilder();
+        for (String arg: args) {
+            builder.append(arg);
+            builder.append("-");
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
+    }
 }
